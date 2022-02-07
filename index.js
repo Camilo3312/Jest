250f00c017a233d76e7e535912a6793fc4d0918f
camilo3312 <ccvelasquez531@misena.edu.co>
2022-02-07 00:21:14 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const Suma = (nuno, ndos) => {
-    return nuno + ndos
+const Suma = (uno, dos) => {
+    return uno + dos
 } 
 
+const InvertirCadena = (cadena) => {
+    return cadena.split('').reverse().toString().replace(/,/g,'')
+} 
+
+const Promedio = valores => {
+    const resutl = (valores.reduce((total, item) => total + item, 0) / valores.length).toString().slice(0,3)
+    return parseFloat(resutl)
+}
+
 module.exports = {
-    Suma
+    Suma,
+    InvertirCadena,
+    Promedio
 }
--- index.test.js
+++ index.test.js
@@ -1,6 +1,16 @@
-const { Suma } = require('./index') 
+const { Suma, InvertirCadena, Promedio } = require('./index') 
 
-test('two plus two is four', () => {
+test('suma basica', () => {
     const result = Suma(2,2)
     expect(result).toBe(4);
+});
+
+test('invertir cadena', () => {
+    const result = InvertirCadena("test")
+    expect(result).toBe("tset");
+});
+
+test('promedio', () => {
+    const result = Promedio([1,4,1,5,2,3])
+    expect(result).toBe(2.6);
 });
